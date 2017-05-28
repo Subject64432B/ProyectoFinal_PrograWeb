4b13ecd26b771cf7cb6bbe4b84d598f87d9e6508
LuisBML <bryan_mtz95@hotmail.com>
2017-05-28 17:15:38 -0600

    Ejemplo reactividad

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -2,3 +2,11 @@
 Template.layout.helpers({
   pageTitle: function() { return Session.get('pageTitle'); }
 });*/
+
+/*Ejemplo reactividad: Configuración de una computación
+Meteor.startup(function() {
+  Tracker.autorun(function() {
+    console.log('There are ' + Posts.find().count() + ' posts');
+  });
+});
+*/
