d54c7caaae9983fe792a6fc3bf84154bb95cea68
BrandonATDeveloper <Brandon.AguirreT@outlook.com>
2017-05-31 10:24:41 -0600

    Tests añadidos al paquete.

--- packages/errors/errors-tests.js
+++ packages/errors/errors-tests.js
@@ -1,11 +1,21 @@
-// Import Tinytest from the tinytest Meteor package.
-import { Tinytest } from "meteor/tinytest";
+Tinytest.add("Errors - collection", function(test) {
+  test.equal(Errors.collection.find({}).count(), 0);
 
-// Import and rename a variable exported by errors.js.
-import { name as packageName } from "meteor/tmeasday:errors";
+  Errors.throw('A new error!');
+  test.equal(Errors.collection.find({}).count(), 1);
 
-// Write your tests here!
-// Here is an example.
-Tinytest.add('errors - example', function (test) {
-  test.equal(packageName, "errors");
+  Errors.collection.remove({});
+});
+
+Tinytest.addAsync("Errors - template", function(test, done) {
+  Errors.throw('A new error!');
+  test.equal(Errors.collection.find({}).count(), 1);
+
+  // render the template
+  UI.insert(UI.render(Template.meteorErrors), document.body);
+
+  Meteor.setTimeout(function() {
+    test.equal(Errors.collection.find({}).count(), 0);
+    done();
+  }, 3500);
 });
