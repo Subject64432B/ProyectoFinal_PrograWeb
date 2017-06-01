a0190c46cc49ef4be00185d7ec0cd7d5d9efff79
BrandonATDeveloper <Brandon.AguirreT@outlook.com>
2017-05-31 19:37:35 -0600

    Añadida la colección de comentarios.

--- server/publications.js
+++ server/publications.js
@@ -7,6 +7,10 @@ Meteor.publish('comments', function(postId) {
   return Comments.find({postId: postId});
 });
 
+Meteor.publish('notifications', function() {
+  return Notifications.find();
+});
+
 /* Publicar los datos a los que el cliente actual puede tener acceso
 Meteor.publish('posts', function(author) {
   return Posts.find({flagged: false, author: author});
