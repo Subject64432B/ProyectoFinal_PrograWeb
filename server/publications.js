12928df09aff4fc1ed2611f189b819559999ea94
BrandonATDeveloper <Brandon.AguirreT@outlook.com>
2017-05-31 18:11:17 -0600

    Mostrar los comentarios en `postPage`.

--- server/publications.js
+++ server/publications.js
@@ -2,11 +2,11 @@ Meteor.publish('posts', function() {
   return Posts.find();
 });
 
+Meteor.publish('comments', function() {
+  return Comments.find();
+});
+
 /* Publicar los datos a los que el cliente actual puede tener acceso
 Meteor.publish('posts', function(author) {
   return Posts.find({flagged: false, author: author});
 });*/
-
-Meteor.publish('comments', function() {  
-  return Comments.find();
-});
