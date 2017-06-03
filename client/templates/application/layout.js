756aa6e9ca7258638186194450dbf7c95fea2746
Mario Guillen <marioagr95@outlook.com>
2017-06-03 16:48:36 -0600

    Transición entre páginas

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -10,3 +10,18 @@ Meteor.startup(function() {
   });
 });
 */
+Template.layout.onRendered(function () {
+  this.find('#main')._uihooks = {
+    insertElement: function (node, next) {
+      $(node)
+        .hide()
+        .insertBefore(next)
+        .fadeIn();
+    },
+    removeElement: function (node) {
+      $(node).fadeOut(function () {
+        $(this).remove();
+      });
+    }
+  }
+});
